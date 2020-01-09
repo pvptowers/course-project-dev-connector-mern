11cd3d14d71e619098f333b0d6fb091592f46af0
Paul <paultowers2005@yahoo.com.au>
2020-01-09 22:53:43 +1100

    redirect user on login or register

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
+
 // import axios from "axios";
 
-const Login = () => {
+const Login = ({ login, isAuthenticated }) => {
   //Our state is formData - the function we use to update our state is setFormData
   const [formData, setFormData] = useState({
     email: "",
@@ -18,8 +22,14 @@ const Login = () => {
 
   const onSubmit = async e => {
     e.preventDefault();
-    console.log("SUCCESS");
+    login(email, password);
   };
+
+  //Redirect user if they are logged in
+
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
   return (
     <Fragment>
@@ -61,4 +71,13 @@ const Login = () => {
   );
 };
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps, { login })(Login);
