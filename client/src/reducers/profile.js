41c7203a518c1ed6d777dbdf1c33c00c952a1981
pvptowers <paultowers2005@yahoo.com.au>
2020-01-15 10:14:38 +1100

    create AddExperience and AddEducation components and associated actions

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -20,11 +20,13 @@ export default function(state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
         loading: false
       };
+
     case PROFILE_ERROR:
       return {
         ...state,
