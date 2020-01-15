594312c2ed724c217796fd25570066bfe602cc8e
pvptowers <paultowers2005@yahoo.com.au>
2020-01-15 11:40:06 +1100

    finalise profile actions and reducer for getting individual user profiles and github repos

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -26,7 +26,12 @@ export default function(state = initialState, action) {
         profile: payload,
         loading: false
       };
-
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false
+      };
     case PROFILE_ERROR:
       return {
         ...state,
@@ -38,6 +43,12 @@ export default function(state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false
+      };
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false
       };
     default:
